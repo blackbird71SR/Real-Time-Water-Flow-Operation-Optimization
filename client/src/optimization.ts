011340426cf91d1fb5f9a0cb4356aaa740d07fd1
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2021-11-13 16:55:32 -0600

    Get sorted array based on ratio

--- client/src/optimization.ts
+++ client/src/optimization.ts
@@ -32,6 +32,39 @@ export type ClientResponse = {
 
 // You should do better!
 export function processRequest(request: ServerRequest): ClientResponse {
+  var flowRateIn = request.flowRateIn;
+  var operations = request.operations;
+
+  var flowRateArray = [];
+  var freeMoney = [];
+
+  for (let i = 0; i < operations.length; i++) {
+    var operation = operations[i];
+    var operationId = operation.id;
+    var revenueStructure = operation.revenueStructure;
+    for (let j = 0; j < revenueStructure.length; j++) {
+
+      if (revenueStructure[j].flowPerDay == 0 && revenueStructure[j].dollarsPerDay > 0) {
+        freeMoney.push({
+          "operationId": operationId,
+          "flowPerDay": revenueStructure[j].flowPerDay,
+          "dollorsperDay":revenueStructure[j].dollarsPerDay,
+          "ratio": revenueStructure[j].dollarsPerDay
+        })
+      } else if (revenueStructure[j].flowPerDay != 0 && revenueStructure[j].dollarsPerDay > 0) {
+        flowRateArray.push({
+          "operationId": operationId,
+          "flowPerDay": revenueStructure[j].flowPerDay,
+          "dollorsperDay":revenueStructure[j].dollarsPerDay,
+          "ratio": revenueStructure[j].dollarsPerDay/ revenueStructure[j].flowPerDay
+        })
+      }
+    }
+  }
+
+  flowRateArray.sort((a, b) => (a.ratio > b.ratio) ? -1 : 1)
+  console.log(flowRateArray);
+  
   const evenDistribution = request.flowRateIn / request.operations.length;
   return request.operations.map(operation => {
     return {
