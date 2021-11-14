71a49a2854632c6c5e467510901148f19cbcc2b5
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2021-11-14 06:12:58 -0600

    Finalize visualizations

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -39,6 +39,17 @@ const useStyles = makeStyles((theme: Theme) =>
   }),
 );
 
+function rename(response: any[]) { // function to rename on button click
+  var responseUpdated = response.map(function(obj) {
+      obj['name'] = obj['operationId'];
+      obj['y'] = obj['flowRate']
+      delete obj['name'];
+      delete obj['y']
+      return obj;
+  });
+  return responseUpdated
+}
+
 function App() {
   const classes = useStyles();
 
@@ -73,10 +84,13 @@ function App() {
       if (data.type === "CURRENT_STATE") {
         const request: ServerRequest = JSON.parse(message.data);
         setRequest(request);
+
         const response = processRequest(request)
-        const responseOriginal = processRequest(request)
+        const responseOriginal = processRequestOriginal(request)
+
         setResponse(response)
         setResponseOriginal(responseOriginal)
+
         ws.send(JSON.stringify(response));
       } else if (data.type === "OPTIMATION_RESULT") {
         const response: ServerResponse = JSON.parse(message.data);
@@ -122,44 +136,56 @@ function App() {
 
   return (
     <div>
-     <AppBar position="static">
+     <AppBar position="static" style={{ background: '#7CB5EC' }}>
         <Toolbar>
           <Typography variant="h5" className={classes.title}>
-            UTD Hackathon
+            Water Flow Dashboard
           </Typography>
         </Toolbar>
       </AppBar>
-      <div className={classes.body}>
+      {/* <div className={classes.body}>
         <div>1.) Server Sends Current State of the System:</div>
         <textarea rows={10} cols={150} value={JSON.stringify(request, undefined, 2)} />
         <div>2.) Client Sends Solution to the Optimization:</div>
         <textarea rows={10} cols={150} value={JSON.stringify(response, undefined, 2)}/>
         <div>3.) Server Sends Result:</div>
         <textarea rows={10} cols={150} value={JSON.stringify(result, undefined, 2)}/>
-      </div>
+      </div> */}
 
       <Grid container spacing={1} style={{textAlign: "center",}}>
         <Grid item xs={12} sm={12} md={4} className='text-center'>
           <Card elevation={6}>
             <CardContent>
-              <h1>Revenue Per Day</h1>
-              <h2>$ {Number((revenuePerDay[revenuePerDay.length-1]).toFixed(2))}</h2>
+              <Typography variant="h3" className={classes.title} style={{textAlign: "center",}}>
+                $ {Number((revenuePerDay[revenuePerDay.length-1]).toFixed(2))}
+              </Typography>
+              <Typography variant="h6" className={classes.title} style={{textAlign: "center",}}>
+                Revenue/Day
+              </Typography>
             </CardContent>
           </Card>
         </Grid>
         <Grid item xs={12} sm={12} md={4}>
           <Card elevation={6}>
             <CardContent>
-              <h1>Average Flow Rate In</h1>
-              <h2>$ {Number((getAvg(flowRateIn)).toFixed(2))}</h2>
+            <Typography variant="h3" className={classes.title} style={{textAlign: "center",}}>
+                {Number((getAvg(flowRateIn)).toFixed(2))}
+              </Typography>
+              <Typography variant="h6" className={classes.title} style={{textAlign: "center",}}>
+              Avg. Flow Rate In (bbls)
+              </Typography>
             </CardContent>
           </Card>
         </Grid>
         <Grid item xs={12} sm={12} md={4}>
           <Card elevation={6}>
             <CardContent>
-              <h1>Average Flow Rate to Operations</h1>
-              <h2>$ {Number((getAvg(flowRateToOperations)).toFixed(2))}</h2>
+            <Typography variant="h3" className={classes.title} style={{textAlign: "center",}}>
+            {Number((getAvg(flowRateToOperations)).toFixed(2))}
+              </Typography>
+              <Typography variant="h6" className={classes.title} style={{textAlign: "center",}}>
+              Avg. Flow Rate to Operations (bbls)
+              </Typography>
             </CardContent>
           </Card>
         </Grid>
@@ -181,28 +207,54 @@ function App() {
               {/* <h1>Pit Capacity</h1> */}
               <ReactHighcharts config = {{
                 chart: {plotBackgroundColor: null,plotBorderWidth: null,plotShadow: false,type: 'pie'},
+                colors: ['#00008b', '#bc8f8f'],
                 tooltip: {pointFormat: '{series.name}: <b>{point.percentage:.4f}%</b>'},
                 accessibility: {point: {valueSuffix: '%'}},
                 title: {text: 'Pit Capacity'},
                 series: [{name: 'Status',colorByPoint: true, data: [{name:'Water', y:currentPitVolume[currentPitVolume.length - 1]}, {name:'Empty', y:maximumPitVolume[maximumPitVolume.length - 1] - currentPitVolume[currentPitVolume.length - 1]}]}], 
               }}></ReactHighcharts>
-              <h2>$ {Number((currentPitVolume[currentPitVolume.length - 1]).toFixed(2))}</h2>
-              <h2>$ {Number((maximumPitVolume[maximumPitVolume.length - 1]).toFixed(2))}</h2>
+              {/* <h2>$ {Number((currentPitVolume[currentPitVolume.length - 1]).toFixed(2))}</h2> */}
+              {/* <h2>$ {Number((maximumPitVolume[maximumPitVolume.length - 1]).toFixed(2))}</h2> */}
             </CardContent>
           </Card>
         </Grid>
-        {/* <Grid item xs={12} sm={12} md={9}>
+        <Grid item xs={12} sm={12} md={9}>
           <Card elevation={6}>
             <CardContent>
-              <ReactHighcharts config = {{
-                title: {text: 'Revenue Structure'},
-                series: [{name: 'Revenue', data: request.operations}], 
-                yAxis:{title: {text: 'Revenue (in $)'}},
-                xAxis:{title: {text: 'Time'}}
-              }}></ReactHighcharts>
+                {response && responseOriginal ? (
+                  <ReactHighcharts config = {{
+                    title: {text: 'Water Distribution'},
+                    series: [{name: 'Our Algo', data: response.map(value => value.flowRate)}, {name: 'Original Algo', data: responseOriginal.map(value => value.flowRate)}], 
+                    yAxis:{title: {text: 'Amount of Water (bbls)'}},
+                    xAxis:{title: {text: 'Operations', categories: response.map(value => value.operationId)}}
+                  }}></ReactHighcharts>
+                ) : (
+                  <ReactHighcharts config = {{
+                    title: {text: 'Water Distribution'},
+                    yAxis:{title: {text: 'Amount of Water (bbls)'}},
+                    xAxis:{title: {text: 'Operations'}}
+                  }}></ReactHighcharts>
+                )}
             </CardContent>
           </Card>
-        </Grid> */}
+        </Grid>
+        <Grid item xs={12} sm={12} md={3}>
+          <Card elevation={6}>
+            <CardContent>
+
+              <ReactHighcharts config = {{
+                chart:{type:'column'},
+                colors: ['#00008b', '#7CB5EC'],
+                title: {text: 'Inflow/ Outflow'},
+                plotOptions: {column: {grouping: false}},
+                series: [{name: 'Inflow', data: flowRateIn.slice(-5)} , {name: 'Outflow', data: flowRateToOperations.slice(-5)}], 
+                yAxis:{title: {text: 'Amount of Water (bbls)'}},
+                xAxis:{title: {text: 'Time'}}
+              }}></ReactHighcharts>
+
+            </CardContent>
+          </Card>
+        </Grid>
       </Grid>
       
       {/* <ReactHighcharts config = {{title: {text: 'revenuePerDay chart'},series: [{name: 'Revenue Per Day', data: revenuePerDay}], yAxis:{title: {text: 'Revenue (in $)'}}}}></ReactHighcharts> */}
